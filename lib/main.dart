171e9bda2451f1e6f11ed95549a32ebc5aaed029
Philipi Guedes <philipi2003@gmail.com>
2026-09-18 00:13:25 -0300

    testando CI

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('O quanto você apertou é o quanto me amas:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
